ca62a333ec6d335d81a33501c4a019273a1d3206
narindra042001 <narindra042001@gmail.com>
2022-05-25 10:02:13 +0300

    Firts Commit

---
+++ assets/js/pages/datatables_data_sources.js
@@ -0,0 +1,19 @@
+/* ------------------------------------------------------------------------------
+*
+*  # Datatables data sources
+*
+*  Specific JS code additions for datatable_data_sources.html page
+*
+*  Version: 1.0
+*  Latest update: Aug 1, 2015
+*
+* ---------------------------------------------------------------------------- */
+
+$(function() {
+
+
+    // Table setup
+    // ------------------------------
+    // HTML sourced data
+    $('.datatable-html').dataTable();
+});
